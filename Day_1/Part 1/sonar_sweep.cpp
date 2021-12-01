90ed09b67e238c984a314a164637fd58388b98e6
Lodewyk-Git <Lodewyk03@gmail.com>
2021-12-01 17:21:56 +0200

    Enter

---
+++ Day_1/Part 1/sonar_sweep.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+int main() {
+    
+    std::ifstream file("values.txt");
+    std::string line;
+
+    int CurrentNumber;
+    int PreviousNumber;
+    int Total;
+
+    Total = -1;
+    PreviousNumber = 0;
+
+    if(file.is_open()){
+        while (getline(file,line))
+        {
+            std::stringstream lineNumber(line);
+            lineNumber >> CurrentNumber;
+            //std::cout << CurrentNumber << "  " << PreviousNumber << '\n';
+            if (PreviousNumber < CurrentNumber)
+            {
+                Total++;
+            }
+            PreviousNumber = CurrentNumber;
+        }
+    std::cout << Total; 
+    };
+}
