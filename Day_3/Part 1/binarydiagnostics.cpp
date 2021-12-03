501cef498c007bafe53de3059dae4e34cf54fb6e
Lodewyk-Git <Lodewyk03@gmail.com>
2021-12-03 22:35:51 +0200

    Enter

---
+++ Day_3/Part 1/binarydiagnostics.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int binaryToDecimal(string n)
+{
+    string num = n;
+    int dec_value = 0;
+ 
+    // Initializing base value to 1, i.e 2^0
+    int base = 1;
+ 
+    int len = num.length();
+    for (int i = len - 1; i >= 0; i--) {
+        if (num[i] == '1')
+            dec_value += base;
+        base = base * 2;
+    }
+ 
+    return dec_value;
+}
+
+int main(){
+
+    ifstream file("values.txt");
+    string line, gammaRate, epsilonRate;
+    int powerConsumption;
+    vector<string> binaryList;
+
+    gammaRate = "";
+    epsilonRate = "";
+
+    if(file.is_open()){
+        while (getline(file,line))
+        {
+            binaryList.push_back(line);
+        }
+    }
+
+    int binaryCount;
+
+    for (int b = 0; b < 12; b++)
+    {
+        binaryCount = 0;
+
+        for (int a = 0; a < binaryList.size(); a++)
+        {
+            if (binaryList[a][b] == '1')
+            {
+                binaryCount++;
+            }
+            else
+            {
+                binaryCount--;
+            }
+        }
+
+        if (binaryCount > 0)
+        {
+            gammaRate += '1';  
+        }
+        else
+        {
+            gammaRate += '0';
+        }
+        
+    }
+
+    for (int c = 0; c < gammaRate.size(); c++)
+    {
+        if (gammaRate[c] == '0')
+        {
+            epsilonRate += '1';  
+        }
+        else
+        {
+            epsilonRate += '0';
+        } 
+    }
+
+    powerConsumption = binaryToDecimal(epsilonRate)*binaryToDecimal(gammaRate);
+
+    cout << gammaRate << " " << epsilonRate << " " << powerConsumption << '\n';
+}//3808
+//287
+
---
+++ Day_3/Part2/binarydiagnostics.cpp
@@ -0,0 +1,241 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+string calculateGammaRate(vector<string> binaryList){
+    
+    int binaryCount;
+    string gammaRate;
+
+    for (int b = 0; b < 12; b++)
+    {
+        binaryCount = 0;
+
+        for (int a = 0; a < binaryList.size(); a++)
+        {
+            if (binaryList[a][b] == '1')
+            {
+                binaryCount++;
+            }
+            else
+            {
+                binaryCount--;
+            }
+        }
+
+        if (binaryCount > 0)
+        {
+            gammaRate += '1';  
+        }
+        else if (binaryCount < 0)
+        {
+            gammaRate += '0';
+        }else
+        {
+            gammaRate += '1';
+        }
+        
+    }
+
+    return gammaRate;
+}
+
+string calculateEpsilonRate(vector<string> binaryList){
+    
+    int binaryCount;
+    string epsilonRate;
+
+    for (int b = 0; b < 12; b++)
+    {
+        binaryCount = 0;
+
+        for (int a = 0; a < binaryList.size(); a++)
+        {
+            if (binaryList[a][b] == '1')
+            {
+                binaryCount--;
+            }
+            else
+            {
+                binaryCount++;
+            }
+        }
+
+        if (binaryCount > 0)
+        {
+            epsilonRate += '1';  
+        }
+        else if (binaryCount < 0)
+        {
+            epsilonRate += '0';
+        } else 
+        {
+            epsilonRate += '0';
+        }
+        
+    }
+
+    return epsilonRate;
+}
+
+int binaryToDecimal(string n)
+{
+    string num = n;
+    int dec_value = 0;
+ 
+    // Initializing base value to 1, i.e 2^0
+    int base = 1;
+ 
+    int len = num.length();
+    for (int i = len - 1; i >= 0; i--) {
+        if (num[i] == '1')
+            dec_value += base;
+        base = base * 2;
+    }
+ 
+    return dec_value;
+}
+
+string calculateOxygen(vector<string> diagnosticList){
+
+    int binaryCount;
+
+    while (diagnosticList.size() > 1)
+    {
+        for (int b = 0; b < 12; b++)
+        {
+            binaryCount = 0;
+            vector<string> commonList;
+            for (int a = 0; a < diagnosticList.size(); a++)
+            {
+                if (diagnosticList[a][b] == '1')
+                {
+                    binaryCount++;
+                }
+                else
+                {
+                    binaryCount--;
+                }
+            }
+
+            if (binaryCount >= 0) //1
+            {
+                for (int c = 0; c < diagnosticList.size(); c++)
+                {
+                    if (diagnosticList[c][b] == '1' && diagnosticList.size() > 1)
+                    {
+                        commonList.push_back(diagnosticList[c]);
+                    }
+                }
+            }
+            else if (binaryCount < 0) // 0
+            {
+                for (int c = 0; c < diagnosticList.size(); c++)
+                {
+                    if (diagnosticList[c][b] == '0' && diagnosticList.size() > 1)
+                    {
+                        commonList.push_back(diagnosticList[c]);
+                    }
+                }
+            }
+            std::cout << diagnosticList.size() << '\n';
+            std::cout << commonList.size() << '\n';
+        diagnosticList = commonList;
+        } 
+    }
+
+    return diagnosticList[0];
+}
+
+string calculateScrubber(vector<string> diagnosticList){
+
+    int binaryCount;
+
+    while (diagnosticList.size() > 1)
+    {
+        
+        for (int b = 0; b < 12; b++)
+        {
+            binaryCount = 0;
+            vector<string> commonList;
+            for (int a = 0; a < diagnosticList.size(); a++)
+            {
+                if (diagnosticList[a][b] == '1')
+                {
+                    binaryCount++;
+                }
+                else
+                {
+                    binaryCount--;
+                }
+            }
+
+            if (binaryCount >= 0) //1
+            {
+                for (int c = 0; c < diagnosticList.size(); c++)
+                {
+                    if (diagnosticList[c][b] == '0' && diagnosticList.size() > 1)
+                    {
+                        commonList.push_back(diagnosticList[c]);
+                    }
+                }
+            }
+            else if (binaryCount < 0) // 0
+            {
+                for (int c = 0; c < diagnosticList.size(); c++)
+                {
+                    if (diagnosticList[c][b] == '1' && diagnosticList.size() > 1)
+                    {
+                        commonList.push_back(diagnosticList[c]);
+                    }
+                }
+            }
+            std::cout << diagnosticList.size() << '\n';
+            std::cout << commonList.size() << '\n';
+        diagnosticList = commonList;
+        } 
+    }
+
+    return diagnosticList[0];
+}
+
+int main(){
+
+    ifstream file("values.txt");
+    string line, gammaRate, epsilonRate, oxygenRate, scrubberRate;
+    int powerConsumption, lifeSupport;
+    vector<string> binaryList;
+
+    if(file.is_open()){
+        while (getline(file,line))
+        {
+            binaryList.push_back(line);
+        }
+    }
+
+    gammaRate = calculateGammaRate(binaryList);
+    epsilonRate = calculateEpsilonRate(binaryList);
+    powerConsumption = binaryToDecimal(epsilonRate)*binaryToDecimal(gammaRate);
+
+    oxygenRate = calculateOxygen(binaryList);
+    scrubberRate = calculateScrubber(binaryList);
+
+    lifeSupport = binaryToDecimal(scrubberRate)*binaryToDecimal(oxygenRate);
+
+    std::cout << "gamma rate: "<< gammaRate << " epsilon rate: " 
+    << calculateEpsilonRate(binaryList) << " power consumption: " 
+    << powerConsumption << "\noxygen generator rating: "
+    << oxygenRate << "\nCO2 scrubber rating: "
+    << scrubberRate << " life support: "
+    << lifeSupport
+    << '\n';
+
+    std::cout << binaryToDecimal(oxygenRate) << " " << binaryToDecimal(scrubberRate);
+}
+//3808
+//287
+
